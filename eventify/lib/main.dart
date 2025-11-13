9e43ba82b678b83f6e5856b3ae019cde4d5eb0c5
Flagn3 <flagn3@gmail.com>
2025-11-13 23:03:41 +0100

    Cambios locales

--- eventify/lib/main.dart
+++ eventify/lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:eventify/Screens/register_screen.dart';
+// import 'package:eventify/Screens/register_screen.dart';
+import 'package:eventify/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const Registerscreen()
+      home: const LoginScreen()
     );
   }
 }
