1e205251527c54c69221a37652a45524505c6f53
santogarrido <santocildesgarridoraul@gmail.com>
2026-01-17 18:34:30 +0100

    Creation of the EventAttendees provider, model and service

---
+++ eventify/lib/services/event_attendees.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'package:eventify/models/event_attendees_response.dart';
+import 'package:http/http.dart' as http;
+
+class EventAttendeesService {
+  static const String _baseUrl = 'https://eventify.iaknowhow.es/public/api';
+
+  //Register Event
+  Future<EventAttendeesResponse> registerEvent(int userId, int eventId, String token) async {
+    Uri url = Uri.parse('$_baseUrl/registerEvent');
+
+    final response = await http.post(
+      url,
+      body: json.encode({'user_id' : userId, 'event_id': eventId, 'registered_at' : DateTime.now()}),
+      headers: {'Accept' : 'application/json', 'Authorization' : 'Bearer $token' , 'Content-Type' : 'application/json'} 
+    );
+
+    final eventAttendessResponse = EventAttendeesResponse.fromJson(json.decode(response.body));
+
+    return eventAttendessResponse;
+  }
+
+    //Unregister Event
+  Future<EventAttendeesResponse> unregisterEvent(int userId, int eventId, String token) async {
+    Uri url = Uri.parse('$_baseUrl/unregisterEvent');
+
+    final response = await http.post(
+      url,
+      body: json.encode({'user_id' : userId, 'event_id': eventId}),
+      headers: {'Accept' : 'application/json', 'Authorization' : 'Bearer $token' , 'Content-Type' : 'application/json'} 
+    );
+
+    final eventAttendessResponse = EventAttendeesResponse.fromJson(json.decode(response.body));
+
+    return eventAttendessResponse;
+  }
+}
